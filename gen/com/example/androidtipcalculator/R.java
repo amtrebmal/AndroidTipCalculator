e77380ff9ec1eac9c9948ea9baf4bf63adde1375
Lambert <lambert@yahoo-inc.com>
2013-06-04 08:40:22 -0700

    working copy, needs some improvements

--- gen/com/example/androidtipcalculator/R.java
+++ gen/com/example/androidtipcalculator/R.java
@@ -26,16 +26,14 @@ public final class R {
     public static final class id {
         public static final int TipAmount=0x7f080003;
         public static final int TipTitle=0x7f080002;
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080006;
         public static final int billText=0x7f080000;
-        public static final int editText1=0x7f080006;
         public static final int fifperc=0x7f080004;
         public static final int tenperc=0x7f080001;
         public static final int twenperc=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_result=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/androidtipcalculator/MainActivity.java
+++ src/com/example/androidtipcalculator/MainActivity.java
@@ -1,67 +1,96 @@
 package com.example.androidtipcalculator;
 
-
-import java.util.ArrayList;
+import com.example.androidtipcalculator.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-	private Button tenpercButton;
-	private Button fifpercButton;
-	private Button twenpercButton;
-
-	private String strBillAmount;
-	private float fltTipAmount;
-	private TextView tvTipAmount;
-
+	EditText BillValue;
+	TextView TipValue;
+	Button Calculate;
+	float num1 , num2;
 	
-	//
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		this.setContentView(R.layout.activity_result);
-					
-		this.tenpercButton = (Button)this.findViewById(R.id.tenperc);
-		this.fifpercButton = (Button)this.findViewById(R.id.fifperc);
-		this.twenpercButton = (Button)this.findViewById(R.id.twenperc);
-		
-		this.tenpercButton.setOnClickListener(new OnClickListener() {
-			
-			
-			@Override
-			public void onClick(View view) {	
-				
-				EditText etbillAmount = (EditText) findViewById(R.id.billText);
-
-				float result = Float.parseFloat(etbillAmount.getText().toString()) * 10/100;
-				
-				EditText etResult = (EditText) findViewById(R.id.TipAmount);
-				etResult.setText(Float.toString(result));
-
-				
-				// billCalculate();
-				// Log.e ("lm1", mBillAmount(v));
-			}
-		});
-	}
 	
-	//Read in bill before tip calculation
-	public void billCalculate () {
-		
-		
-		
-		
-	}
+	Button tenpercButton;
+	Button fifpercButton;
+	Button twenpercButton;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_main);
+        
+		tenpercButton = (Button)this.findViewById(R.id.tenperc);
+		fifpercButton = (Button)this.findViewById(R.id.fifperc);
+		twenpercButton = (Button)this.findViewById(R.id.twenperc);
+
+        	BillValue = (EditText) findViewById(R.id.billText);
+        
+        	TipValue = (TextView) findViewById(R.id.TipAmount);
+        	TipValue.setText("0.00");
+
+
+        	//Adding listener to 1st button
+        	tenpercButton.setOnClickListener(new OnClickListener() {
+
+        		public void onClick(View v) {
+        			//Getting tip and setup value and passing to show result
+        			showResultTen(BillValue.getText());
+        		}
+        	});
+        	
+        	//Adding listener to 2nd button
+        	fifpercButton.setOnClickListener(new OnClickListener() {
+
+        		public void onClick(View v) {
+        			//Getting tip and setup value and passing to show result
+        			showResultFifteen(BillValue.getText());
+        		}
+        	});
+        	
+        	//Adding listener to 3rd button
+        	twenpercButton.setOnClickListener(new OnClickListener() {
+
+        		public void onClick(View v) {
+        			//Getting tip and setup value and passing to show result
+        			showResultTwenty(BillValue.getText());
+        		}
+        	});
+    }
+	
+	 //Showing multiply results
+	 protected void showResultTen(Editable first) 
+	 {
+		 float num1 = Float.parseFloat(first.toString());
+		 float result = (float) (num1 * 0.10);
+		 TipValue.setText(String.valueOf(result));
+	 }
+	 
+	 //Showing multiply results
+	 protected void showResultFifteen(Editable first) 
+	 {
+		 float num1 = Float.parseFloat(first.toString());
+		 float result = (float) (num1 * 0.15);
+		 TipValue.setText(String.valueOf(result));
+	 }
+	 
+	 //Showing multiply results
+	 protected void showResultTwenty(Editable first) 
+	 {
+		 float num1 = Float.parseFloat(first.toString());
+		 float result = (float) (num1 * 0.20);
+		 TipValue.setText(String.valueOf(result));
+	 }
 	
 	
 
